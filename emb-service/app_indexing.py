a787e09dd25a697ad3fd76cf676e3d7fc4248e49
Bayar Park <bayar342@gmail.com>
2024-06-16 01:52:53 +0700

    feat: template

--- emb-service/app_indexing.py
+++ emb-service/app_indexing.py
@@ -1,9 +1,12 @@
 import shutil
 import tempfile
+from pathlib import Path
+
+from minio import Minio
 
 from kafka_conn import KafkaProducer, KafkaConsumer
 from processing import *
-from .utils import download_content
+from utils import download_content
 
 
 def main():
@@ -30,17 +33,24 @@ def main():
         )
     )
 
-    temp_dir = tempfile.mkdtemp()
-    try:
-        video_id = ...
-        video_path = download_content(...) # fixme
-        checking_pipeline.run(video_path, video_id)
-        result = checking_pipeline.run(video_path, video_id) # fixme
+    minio_client = Minio(
+        'minio:9000',                # fixme
+        'ROOTUSERNAME',            # fixme
+        'ROOTPASSWORD'             # fixme
+    )
 
+    tmp_dir = Path(tempfile.mkdtemp())
+    try:
+        json_data = ...
+        video_id = json_data['uuid']
+        video_path = download_content(
+            minio_client, json_data['bucket_name'], json_data['video_name'], tmp_dir
+        )
+        checking_pipeline.run(video_path, video_id)
     except Exception as e:
         raise  # report to backend
     finally:
-        shutil.rmtree(temp_dir)
+        shutil.rmtree(tmp_dir)
 
 
 if __name__ == '__main__':
--- emb-service/utils.py
+++ emb-service/utils.py
@@ -1,3 +1,15 @@
+from pathlib import Path
 
-def download_content(*args):
-    pass
+from minio import Minio
+from loguru import logger
+
+
+def download_content(
+    client: Minio,
+    bucket_name: str,
+    video_name: str,
+    tmp_dir: Path
+):
+    local_file = str(tmp_dir / Path(video_name).name)
+    client.fget_object(bucket_name, video_name, local_file)
+    logger.info(f"Successfully downloaded {video_name}")
