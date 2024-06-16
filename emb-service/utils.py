93fc52be3dc35a94571577e0b0586597c091599e
Bayar Park <bayar342@gmail.com>
2024-06-16 16:32:10 +0700

    bugfix: fixed utils

--- emb-service/utils.py
+++ emb-service/utils.py
@@ -9,7 +9,8 @@ def download_content(
     bucket_name: str,
     video_name: str,
     tmp_dir: Path
-):
+) -> str:
     local_file = str(tmp_dir / Path(video_name).name)
     client.fget_object(bucket_name, video_name, local_file)
     logger.info(f"Successfully downloaded {video_name}")
+    return local_file
